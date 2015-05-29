d17cde5ed9987b2cf25d88b7e91e6dbc7d1abfa2
littlewing <alexandre@touret.info>
2015-05-29 11:44:29 +0200

    Refactoring

--- src/test/java/info/touret/googledriveclient/LocalFileHelperTest.java
+++ src/test/java/info/touret/googledriveclient/LocalFileHelperTest.java
@@ -2,33 +2,41 @@ package info.touret.googledriveclient;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
+import java.nio.file.FileSystem;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by touret-a on 22/05/2015.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class LocalFileHelperTest {
 
     private LocalFileHelper localFileHelper;
-    private Path rootPath;
+    @Mock
+    private Path mockRootPath;
+
+    @Mock
+    private FileSystem mockFileSystem;
 
     @Before
     public void setUp() throws Exception {
-        rootPath = Paths.get("d:/Tmp");
+        when(mockRootPath.getFileSystem()).thenReturn(mockFileSystem);
         localFileHelper = new LocalFileHelper();
     }
 
     @Test
     public void testlistRecenFilesFromAFolder_Empty() throws Exception {
-        List<File> fileList=localFileHelper.listRecentFilesFromAFolder(rootPath, Instant.now());
+        List<File> fileList=localFileHelper.listRecentFilesFromAFolder(mockRootPath, Instant.now());
         assertTrue(fileList.isEmpty());
-                
     }
 }
