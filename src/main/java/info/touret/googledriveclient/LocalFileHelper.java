b11c13ccdd6917a7895eb00b8c1c17eb24e46948
touret-a <alexandre.touret@sihm.fr>
2015-06-08 14:29:05 +0200

    Doc

--- src/main/java/info/touret/googledriveclient/LocalFileHelper.java
+++ src/main/java/info/touret/googledriveclient/LocalFileHelper.java
@@ -13,11 +13,19 @@ import java.util.logging.Logger;
 import static java.util.logging.Level.SEVERE;
 
 /**
+ * Helper class for local files
  * Created by touret-a on 22/05/2015.
  */
 public class LocalFileHelper {
     private final static Logger LOGGER = Logger.getLogger(LocalFileHelper.class.getName());
 
+    /**
+     * Lists recent files of the corresponding folder
+     *
+     * @param path    the folder to check
+     * @param instant the time to check
+     * @return Corresponding files
+     */
     public List<File> listRecentFilesFromAFolder(Path path, Instant instant) {
         List<File> files = new ArrayList<>();
         try {
@@ -31,6 +39,13 @@ public class LocalFileHelper {
         return files;
     }
 
+    /**
+     * Returns and creates if needed the corresponding folder
+     *
+     * @param rootFolder
+     * @param newFolder
+     * @return The Path of the folder
+     */
     public Path createOrGetFolder(Path rootFolder, String newFolder) {
         Path folder = null;
         try {
